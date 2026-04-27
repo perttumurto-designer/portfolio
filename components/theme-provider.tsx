07185b24bccbbcce977449949550db26b701ff4b
perttumurto-designer <perttu.murto@gmail.com>
2026-04-27 16:23:56 +0300

    darkmode default

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -11,7 +11,7 @@ function ThemeProvider({
     <NextThemesProvider
       attribute="class"
       defaultTheme="dark"
-      enableSystem
+      enableSystem={false}
       disableTransitionOnChange
       {...props}
     >
